d379d9ba9d26071be714a009035bfee276a64fa1
hoon <wjh2144@gmail.com>
2022-07-11 18:00:41 +0900

    hoon

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+import certifi
