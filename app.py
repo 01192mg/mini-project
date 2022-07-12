73f8dfd2f86fee36dcf8c7cb38f50055b0ad6d9d
hoon <wjh2144@gmail.com>
2022-07-12 19:50:49 +0900

    hoon

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from pymongo import MongoClient
+import requests
 
 app = Flask(__name__)
 
-import requests
-from bs4 import BeautifulSoup
+client = MongoClient('3.38.210.185', 27017, username="text", password="sparta")
+db = client.dbsparta
 
-from pymongo import MongoClient
-import certifi
+
+@app.route('/')
+def home():
+    return render_template("mainpage.html")
+
+
+@app.route('/post', methods=['POST'])
+def save_post():
+    title_receive = title.form
+    definition_receive = request.form['definition_give']
+    doc = {'word': word_receive, 'definition': definition_receive}
+    db.word.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': f'단어 {word_receive} 저장'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
