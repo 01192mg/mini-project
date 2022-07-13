c013cd02070fc419ed9eeb9288c0ef3e16357266
woojangsan <oxorr_0424@naver.com>
2022-07-13 09:29:24 +0900

    3rd

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 
-
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
-
+# client = MongoClient('3.38.210.185', 27017, username="text", password="sparta")
+# db = client.dbsparta
 client = MongoClient('mongodb+srv://yongwook:Dnr4fkdgo@cluster0.yb6vivj.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta_plus_week4
 
@@ -22,12 +22,37 @@ def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-
-        return render_template('index.html')
+        return render_template('mainpage.html')
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+
+
+@app.route('/post', methods=['POST'])
+def save_post():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+
+        title_receive = request.form["title_give"]
+        description_receive = request.form["description_give"]
+        image_receive = request.form["image_give"]
+
+        doc = {
+            "username": user_info["username"],
+            "title": title_receive,
+            "description": description_receive,
+            "image": image_receive
+        }
+        db.posts.insert_one(doc)
+        return jsonify({"result": "success", 'msg': '작성완료'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
 
 
 @app.route('/login')
@@ -61,8 +86,8 @@ def sign_in():
 
     if result is not None:
         payload = {
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
@@ -71,21 +96,23 @@ def sign_in():
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
+
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-        "profile_pic": "",                                          # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        "profile_info": ""                                          # 프로필 한 마디
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+        "profile_pic": "",  # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
+        "profile_info": ""  # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
+
 
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
@@ -118,13 +145,8 @@ def posting():
 
 @app.route("/get_posts", methods=['GET'])
 def get_posts():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        # 포스팅 목록 받아오기
-        return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다."})
-    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return redirect(url_for("home"))
+    posts = list(db.posts.find({}, {'_id': False}))
+    return jsonify({'posts':posts})
 
 
 @app.route('/update_like', methods=['POST'])
--- static/js/mainpage.js
+++ static/js/mainpage.js
@@ -1,3 +1,8 @@
+$(document).ready(function () {
+    listing();
+});
+
+
 function open_box() {
     $("#mymodal").show();
 }
@@ -5,4 +10,59 @@ function open_box() {
 function close_box() {
     $("#mymodal").hide();
 }
+
+function in_modal() {
+    $("#myin_modal").show();
+}
+
+
+function save_post() {
+    let title = $('#u_title').val()
+    let description = $('#u_content').val()
+    let image = $('#img-url').val()
+
+    $.ajax({
+        type: "POST",
+        url: `post`,
+        data: {
+            title_give: title,
+            description_give: description,
+            image_give: image
+        },
+        success: function (response) {
+            alert(response["msg"])
+            window.location.reload()
+        }
+    });
+}
+
+function listing() {
+    $.ajax({
+        type: 'GET',
+        url: `/get_posts`,
+        data: {},
+        success: function (response) {
+            console.log(response)
+            let rows = response['posts']
+            for (let i = 0; i < rows.length; i++) {
+                let username = rows[i]['username']
+                let image = rows[i]['image']
+                let temp_html = `<div class="card-list">
+                                    <h5 class="name">${username}</h5>
+                                    <button type="button" class="list" onclick="in_modal()"><img src='${image}'width="400" height="400"></button>
+                                </div>`
+                $('.cards-box').append(temp_html)
+            }
+        }
+    })
+}
+
+
+function logout() {
+    ('mytoken').remove;
+    alert('로그아웃!')
+    window.location.href = '/login'
+}
+
+
 
